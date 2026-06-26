06fe84ddb4755baf9820bb18f9691cedad9688af
saif308320 <saifullahowner301@gmail.com>
2026-06-26 15:46:11 +0500

    fix: remove unused useLocation import

--- all-panel-react/src/pages/Home/Home.tsx
+++ all-panel-react/src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Footer from '../../components/Footer/Footer';
